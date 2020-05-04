ed7c03fbbd7a7fe070bceb324113ce035484eb5d
Marek Moskwa. Kurs CoTenFrontend <wtf@vize.pl>
2020-05-04 19:33:33 +0200

    homepage title change & js comment

--- main.js
+++ main.js
@@ -6,5 +6,7 @@ const roznica = age - ageZony;
 console.log (`Nazywam się ${firstName} i mam prawie ${age} lat.\nMoja żona ma na imię ${imieZony} i ma ${ageZony} lat.
 Jestem ${roznica} lata starszy od żony.` );
 
+/* 
 alert (`Cześć, mam na imię ${firstName}. 
 Zaglądnij do konsoli aby zobaczyć więcej informacji o mnie.`);
+*/
