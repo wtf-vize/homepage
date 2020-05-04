33aace3c6451978ca49b9a8b7db7ec5f1819cd6b
Marek Moskwa. Kurs CoTenFrontend <wtf@vize.pl>
2020-05-04 19:26:12 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const firstName ='Marek';
+const imieZony ='Magda';
+const age = 40;
+const ageZony = 38;
+const roznica = age - ageZony;
+console.log (`Nazywam się ${firstName} i mam prawie ${age} lat.\nMoja żona ma na imię ${imieZony} i ma ${ageZony} lat.
+Jestem ${roznica} lata starszy od żony.` );
+
+alert (`Cześć, mam na imię ${firstName}. 
+Zaglądnij do konsoli aby zobaczyć więcej informacji o mnie.`);
