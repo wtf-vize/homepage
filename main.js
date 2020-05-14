efcd4dfa34dc650d807692b1f5fa7445ae7fbc7b
Marek Moskwa. Kurs CoTenFrontend <wtf@vize.pl>
2020-05-14 21:07:04 +0200

    zmiany po lekcji JS, DOM i BEM

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const firstName ='Marek';
-const wifeName ='Magda';
+const firstName = "Marek";
+const wifeName = "Magda";
 const myAge = 40;
 const wifeAge = 38;
 const roznica = myAge - wifeAge;
-console.log (`Mam na imię ${firstName} i mam prawie ${myAge} lat.\nMoja żona ma na imię ${wifeName} i ma ${wifeAge} lat.
-Jestem ${roznica} lata starszy od żony, a te informacje zostały wypisane przy użyciu skryptu main.js.` );
+console.log(`Mam na imię ${firstName} i mam prawie ${myAge} lat.\nMoja żona ma na imię ${wifeName} i ma ${wifeAge} lat.
+Jestem ${roznica} lata starszy od żony, a te informacje zostały wypisane przy użyciu skryptu main.js.`);
 
-
-// alert (`Cześć, mam na imię ${firstName}. 
+// alert (`Cześć, mam na imię ${firstName}.
 // Zaglądnij do konsoli aby zobaczyć więcej informacji o mnie.`);
+
+
+const beret = document.querySelector('.main_heading--js')
+console.log  (beret)
+beret.innerHTML = 'oj'
